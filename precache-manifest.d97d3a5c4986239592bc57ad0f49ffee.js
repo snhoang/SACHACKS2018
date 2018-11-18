509674e7ce3e20a87ca74572198f879ddc7914a4
navalta3030 <navalta3030@gmail.com>
2018-11-17 20:59:45 -0800

    Updates

---
+++ precache-manifest.d97d3a5c4986239592bc57ad0f49ffee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d6625c3522f1797795",
+    "url": "/SACHACKS/static/js/runtime~main.e7d6625c.js"
+  },
+  {
+    "revision": "4b812ea67b1700c36f87",
+    "url": "/SACHACKS/static/js/main.4b812ea6.chunk.js"
+  },
+  {
+    "revision": "1d8ef5f9d05282af9978",
+    "url": "/SACHACKS/static/js/1.1d8ef5f9.chunk.js"
+  },
+  {
+    "revision": "4b812ea67b1700c36f87",
+    "url": "/SACHACKS/static/css/main.40d3d1ad.chunk.css"
+  },
+  {
+    "revision": "8fc608a9c78879b0523178cf720a6276",
+    "url": "/SACHACKS/index.html"
+  }
+];
