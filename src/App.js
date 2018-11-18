183a4fcf7374ae5099dd11040e895f6a2f4e738e
navalta3030 <navalta3030@gmail.com>
2018-11-18 06:00:16 -0800

    slick

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ class App extends Component {
     super(props);
     // Don't call this.setState() here!
     this.state = { 
-      haveData: true
+      haveData: true,
+      Registered: false,
+
     };
   }
 
   sendVoteToServer = (image) => {
     // let url = 'http://127.0.0.1:8000';
     let url = 'http://badass.ghdom.tk/searchVoterDB.php';
-    let bday = document.querySelector('#bday');
+    // let bday = document.querySelector('#bday');
     this.setState({haveData: false})
     fetch(url, {
       method: "POST", 
@@ -25,26 +27,38 @@ class App extends Component {
           "Access-Control-Allow-Origin": '*',
       },
       // credentials: "include",
-      body: JSON.stringify({
-        "image" : image,
-        "bday" : bday.value
-      })
+      // body: JSON.stringify({
+      //   "image" : image,
+      //   "bday" : bday.value
+      // })
+      body: JSON.stringify(image)
   })
   .then( res => res.json() )
   .then(response => {
-    // this.setState({haveData: response})
-    console.log(response);
+    this.setState({haveData: response.status})
+    console.log(this.state);
+    if(response.status === "V") {
+      window.location.href = "http://badass.ghdom.tk/vote.php?id="+response.id;
+    } else {
+      window.location.href = "http://badass.ghdom.tk/index.php?status=R&photo="+response.photo+"&id="+response.id;
+    }
   })
   .catch( err => console.log(err))
 }
 
   render() {
-    const { haveData } = this.state;
+    const { haveData, attempt } = this.state;
     return (
       <div className="App">
+        <img className="ourLogo" alt="logo" src="https://scontent.fmcc1-1.fna.fbcdn.net/v/t1.15752-9/46355562_2201807040057565_8309935061109571584_n.png?_nc_cat=103&_nc_ht=scontent.fmcc1-1.fna&oh=9459aa81b7c09a18dda612d38c0462b4&oe=5C82C491"></img>
+      {
+        attempt
+          ? <h1>TRY AGAIN MY MAN !</h1>
+          : null
+      }
       {
         haveData
-        ? <Camera sendVoteToServer={this.sendVoteToServer}></Camera>
+        ? <Camera className="shadow-2" sendVoteToServer={this.sendVoteToServer}></Camera>
         : <img className="loading-logo" alt="loading" src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"></img>
       }       
       
--- src/Components/Camera/Camera.js
+++ src/Components/Camera/Camera.js
@@ -1,15 +1,27 @@
 import React, {Component} from "react";
 import Webcam from "react-webcam";
+import DatePicker from "react-datepicker";
 
-import './Camera.css'
+import "react-datepicker/dist/react-datepicker.css";
+
+import './Camera.css';
+import 'tachyons';
 
 
 
 class WebcamCapture extends Component {
-    // componentDidMount(){
-    //   let elems = document.querySelectorAll('.datepicker');
-    //   M.Datepicker.init(elems, null)
-    // }
+    constructor(props) {
+      super(props);
+      this.state = {
+        startDate: new Date()
+      };
+    }
+
+    handleChange = (date) => {
+      this.setState({
+        startDate: date
+      });
+    }
 
     setRef = webcam => {
       this.webcam = webcam;
@@ -29,21 +41,33 @@ class WebcamCapture extends Component {
         facingMode: "user"
       };
       return (
-        <div className="webcam">
-          <Webcam
-            audio={true}
-            height={600}
-            ref={this.setRef}
-            screenshotFormat="image/jpeg"
-            width={800}
-            videoConstraints={videoConstraints}
+        <div>
+          <div className="webcam">
+            <Webcam
+              audio={true}
+              height={600}
+              ref={this.setRef}
+              screenshotFormat="image/jpeg"
+              width={800}
+              videoConstraints={videoConstraints}
+              className="camera"
+            />
+          </div>
+          <div className="pa4-l">
+            <form className="bg-transparent mw7 center pa4 br2-ns ba b--black-10 shadow-2">
+              <fieldset className="cf bn ma0 pa0">
+                <div className="cf flex justify-around">
+                <label className="clip" htmlor="email-address">BirthDate</label>
+                <DatePicker className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="12/30/1997" type="text" name="email-address" id="email-address" style={{width: "560px"}}
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                />
+                <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Submit" onClick={this.capture}/>
+                </div>
+              </fieldset>
+            </form>
+          </div>
 
-            className="camera"
-          />
-          <input type="date" id="bday"></input>
-          <button className="btn waves-effect waves-light" onClick={this.capture}>Submit
-            <i className="material-icons right"></i>
-          </button>
         </div>
       );
     }
